090cefc48ae62eb0efd6847e1367dc939f0859a9
emildev666 <emilg.242@gmail.com>
2022-06-19 14:38:23 +0400

    qezel

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,55 +1,114 @@
 // read more
-const paragraph = document.querySelector('p');
-const style = getComputedStyle(paragraph);
-const elementHeight = style.height;
-const heightNumber = parseInt(elementHeight);
+
 const readMore = document.querySelector('.read-more');
 const mt = document.getElementById('more-text');
-let mtData = mt.getAttribute('val');
+// let mtData = mt.getAttribute('val');
 const dropdownArrow = document.querySelector('.dropdown-arrow');
 const dropdownToggle = document.querySelector('.dropdown-toggle');
 const coll = document.getElementsByClassName("collapsible");
-var i;
+let i;
+
+//carousel
+const sliderArea = document.getElementById('slider-area')
+const slider_content = document.querySelectorAll('.slider-content')
+const slideContainer = document.querySelector('.slideContainer')
+const rightArr = document.getElementById('right-arr')
+const leftArr = document.getElementById('left-arr')
+sliderArea.scrollTo(sliderArea.scrollWidth,0)
+let sliderAreaChildrens = sliderArea.childNodes
 
 let text = '';
 let showMore = true;
 
-function getValue(e){
-  const mt = e.value;
-  console.log(mt);
 
+//carousel
+
+if(slider_content.length > 3){
+  sliderArea.style.maxWidth = '1200px'
+  slideContainer.style.maxWidth = '1297px'
+  // if(screen.width > 768){
+  //     sliderArea.style.maxWidth = '1200px'
+  //     slideContainer.style.maxWidth = '1297px'
+  // } else {
+  //     sliderArea.style.maxWidth = '768px'
+  //     // slideContainer.style.maxWidth = '1297px'
+  // }
+  // sliderArea.style.backgroundColor = 'red'
+} else {
+  sliderArea.style.maxWidth = '850px'
+  slideContainer.style.maxWidth = '947px'
+  // sliderArea.style.backgroundColor = 'blue'
 }
 
-function createBtn(more) {
-  let btn = document.createElement('a');
-  btn.classList.add('active')
-  btn.classList.add('read-more')
-  btn.style.cursor="pointer";
-  btn.addEventListener('click', myFunc);
-  btn.innerText = more ? '  Read more' : '  Read less';
-  return btn;
+function createSlideContentPre(x){
+  const slideDiv = document.createElement('div')
+  slideDiv.classList.add('slider-content')
+  slideDiv.setAttribute('style', 'min-width: 0px;')
+  const slideImg = document.createElement('img')
+  slideImg.src = x
+
+  slideDiv.appendChild(slideImg)
+  sliderArea.prepend(slideDiv)
 }
 
-function myFunc() {
+function createSlideContentAppe(x){
+  const slideDiv = document.createElement('div')
+  slideDiv.classList.add('slider-content')
+  slideDiv.setAttribute('style', 'min-width: 0px; transform: translateX(300px);')
+  const slideImg = document.createElement('img')
+  slideImg.setAttribute('style', 'width: 100%;')
+  slideImg.src = x
 
-  if (showMore) {
-    mt.innerText = text;
-    mt.appendChild(createBtn(false))
-    showMore = false;
-
-  } else {
-    showMore = true;
-    mt.innerText = text.substring(0, mtData);
-    mt.appendChild(createBtn(true))
-  }
+  slideDiv.appendChild(slideImg)
+  sliderArea.appendChild(slideDiv)
 }
 
-text = mt.innerText
-mt.innerText = text.substring(0, mtData)
-if (text.length > mtData)
-  mt.appendChild(createBtn(true))
 
-// read more
+rightArr.addEventListener('click', () => {
+  // console.log('right')
+  sliderArea.scrollTo(400,0)
+
+  rightArr.setAttribute('style', 'pointer-events: none;')
+
+  sliderArea.children[sliderArea.children.length - 2].setAttribute('style', ' position: relative; z-index: 1; object-fit:cover; height: 300px; margin-right: 0;')
+  sliderArea.children[sliderArea.children.length - 2].children[0].setAttribute('style', 'filter: blur(0px); width: 100%; ')
+
+  sliderArea.children[sliderArea.children.length - 1].setAttribute('style', ' position: relative; z-index: 0; height: 265px; min-width: 0px !important; margin:0px;')
+  sliderArea.children[sliderArea.children.length - 1].children[0].setAttribute('style', 'filter: blur(10px) grayscale(100); width: 80%; gap: 0px; margin:0;')
+
+
+  setTimeout(() => {
+    sliderArea.children[sliderArea.children.length - 1].remove()
+    rightArr.setAttribute('style', 'pointer-events: all;')
+  }, 300);
+
+
+  createSlideContentPre(sliderArea.children[sliderArea.children.length - 1].children[0].src)
+  setTimeout(() => {
+    sliderArea.children[0].setAttribute('style', 'min-width: 350px;')
+  }, 10);
+  sliderArea.scrollTo(sliderArea.scrollWidth,0)
+})
+
+
+leftArr.addEventListener('click', () => {
+
+  sliderArea.children[sliderArea.children.length - 1].setAttribute('style', 'height: 265px; min-width: 350px; ')
+  sliderArea.children[sliderArea.children.length - 1].children[0].setAttribute('style', 'filter: blur(10px) grayscale(100); width: 320;')
+  createSlideContentAppe(sliderArea.children[0].children[0].src)
+
+  setTimeout(() => {
+    sliderArea.children[sliderArea.children.length - 1].setAttribute('style', 'min-width: 350px; height:300px; object-fit:cover; margin-right: 0;')
+    setTimeout(() => {
+      sliderArea.scrollTo(sliderArea.scrollWidth,0)
+    }, );
+  }, 10);
+  sliderArea.children[0].remove()
+})
+
+
+
+
 //dropdown
 function dropdownchange() {
 
@@ -94,3 +153,47 @@ $(document).ready(function(){
     });
   });
 });
+
+
+
+
+
+
+
+
+function getValue(e){
+  const mt = e.value;
+  console.log(mt);
+
+}
+
+function createBtn(more) {
+  let btn = document.createElement('a');
+  btn.classList.add('active')
+  btn.classList.add('read-more')
+  btn.style.cursor="pointer";
+  btn.addEventListener('click', myFunc);
+  btn.innerText = more ? '  Read more' : '  Read less';
+  return btn;
+}
+
+function myFunc() {
+
+  if (showMore) {
+    mt.innerText = text;
+    mt.appendChild(createBtn(false))
+    showMore = false;
+
+  } else {
+    showMore = true;
+    mt.innerText = text.substring(0, 1250);
+    mt.appendChild(createBtn(true))
+  }
+}
+
+text = mt.innerText
+mt.innerText = text.substring(0, 1250)
+if (text.length > 1250)
+  mt.appendChild(createBtn(true))
+
+// read more
